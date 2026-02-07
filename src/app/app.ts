bb248b4ffc71b1edcaa90aa9d03a4dcc25cef0af
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-02-07 14:24:39 +0530

    add support multiple environments

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
+import { JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Angular Nested Routing With Guards');
+
+  protected readonly environment;
+  constructor() {
+    console.log('App initialized');
+    console.log('API URL:', environment.apiUrl);
+    this.environment = environment;
+  }
 }
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://staging.api.example.com',
+  //apiUrl: 'http://localhost:8080',
+};
